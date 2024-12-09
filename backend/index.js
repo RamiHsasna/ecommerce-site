d5b9e165faab21a4f1af6e39fe40fc9b474cf281
ramihsasna <ramihsasna34@gmail.com>
2024-12-09 10:52:18 +0100

    Add new files and configurations for various dependencies in the backend

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,37 @@
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+const userSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: String,
+    password: String,
+  },
+  {
+    collection: "ecommerce-users",
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
 app.get("/users", async (req, res) => {
   try {
-    const respone = await axios.get("https://fakestoreapi.com/users?limit=10");
-    res.json(respone.data);
+    const users = await User.find();
+    res.json(users);
   } catch (error) {
     res.status(500).json({ error: "Failed to fetch users" });
   }
