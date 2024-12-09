e1dbcdb3f8dce20b5cff6a3eaa583b778d63f4f8
ramihsasna <ramihsasna34@gmail.com>
2024-12-09 18:12:31 +0100

    Remove MongoDB connection logic from backend index file

--- backend/index.js
+++ backend/index.js
@@ -8,14 +8,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("MongoDB connection error:", err));
-
 const User = mongoose.model("User", UserModel);
 
 app.get("/users", async (req, res) => {
